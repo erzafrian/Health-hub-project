7a266a1476984cc32d855add6a85a542886fe754
erzafrian <erzafadil@gmail.com>
2024-10-10 01:35:05 +0700

    Beranda fix

--- js/healthub.js
+++ js/healthub.js
@@ -6,7 +6,8 @@ function nextSlide(n) {
 }
 
 function showSlide(n) {
-  let listImage = document.getElementsByClassName('');
+  let listImage = document.getElementsByClassName('mair');
   console.log(listImage);
 
-  if (n > listImage.length) indexSlide = 1;}
+  if (n > listImage.length) indexSlide = 1;
+}
